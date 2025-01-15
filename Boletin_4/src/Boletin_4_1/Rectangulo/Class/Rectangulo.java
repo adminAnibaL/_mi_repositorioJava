c1176244e98cac00004faa83058a7d8fed25c9c0
anibaL <a88anibal@gmail.com>
2025-01-15 17:13:18 +0100

    Mi segundo commit

---
+++ Boletin_4/src/Boletin_4_1/Rectangulo/Class/Rectangulo.java
@@ -0,0 +1,4 @@
+package Boletin_4_1.Rectangulo.Class;
+
+public class Rectangulo {
+}
---
+++ Boletin_4/src/Boletin_4_1/Rectangulo/Main/Main.java
@@ -0,0 +1,7 @@
+package Boletin_4_1.Rectangulo.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
