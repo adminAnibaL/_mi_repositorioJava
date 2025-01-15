1a1c7decda6ab94ee15689ab974d6c433c8a18b9
anibaL <a88anibal@gmail.com>
2025-01-15 17:17:33 +0100

    Tercer commit

---
+++ Boletin_4/src/Boletin_4_1/CuentaCorriente/Class/CuentaCorriente.java
@@ -0,0 +1,4 @@
+package Boletin_4_1.CuentaCorriente.Class;
+
+public class CuentaCorriente {
+}
---
+++ Boletin_4/src/Boletin_4_1/CuentaCorriente/Main/Main.java
@@ -0,0 +1,7 @@
+package Boletin_4_1.CuentaCorriente.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Soy una Cuenta corriente");
+    }
+}
